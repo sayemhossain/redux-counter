3b715cec57ccc7ebff6b7ba9fff1a1c7f6b3c111
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-22 23:49:16 +0600

    new code added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,8 @@
+import { useSelector } from "react-redux";
+
 export default function Counter() {
+  const count = useSelector((state) => state.value);
+
   return (
     <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
       <div className="text-2xl font-semibold">{count}</div>
