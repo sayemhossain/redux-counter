c53c84fa63103a64f4d98d7209d4c5e5985f9c73
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-22 23:21:42 +0600

    counter reducer done

--- src/redux/counter/counterReducer.js
+++ src/redux/counter/counterReducer.js
@@ -1,4 +1,22 @@
+import { INCREMENT, DECREMENT } from "./actionTypes";
+
 const initialState = {
   value: 0,
 };
-const counterReducer = (state = initialState, action) => {};
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        value: state.value + 1,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        value: state.value - 2,
+      };
+
+    default:
+      return state;
+  }
+};
