8c018b40a3cfb31d99b4fef8a506f7b1d7f6c5bf
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-23 23:59:10 +0600

    New cdoe aded

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,6 @@ import counterReducer from "./counter/counterReducer";
 import myLogger from "./middlewares/myLogger";
 import logger from "redux-logger";
 
-const store = createStore(counterReducer, applyMiddleware(logger));
+const store = createStore(counterReducer, applyMiddleware(logger, myLogger));
 
 export default store;
