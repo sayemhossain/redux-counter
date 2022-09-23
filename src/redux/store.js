f7a1f146c80ebc36b116fe00ef12ac3f1904fd19
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-23 23:56:42 +0600

    new dcodeaded

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 import counterReducer from "./counter/counterReducer";
 import myLogger from "./middlewares/myLogger";
+import logger from "redux-logger";
 
-const store = createStore(counterReducer, applyMiddleware(myLogger));
+const store = createStore(counterReducer, applyMiddleware(logger));
 
 export default store;
