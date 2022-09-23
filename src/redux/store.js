dd38bb1a44a0cc87a881a7bdc0ea992781400a19
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-23 23:35:57 +0600

    NEw cdoeadded

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,16 @@ import { applyMiddleware, createStore } from "redux";
 import counterReducer from "./counter/counterReducer";
 
 //create our first middleware
-const myLogger = (store) => (next) => (action) => {};
+const myLogger = (store) => (next) => (action) => {
+  console.log(`Action: ${JSON.stringify(action)}`);
+  console.log(`Before: ${JSON.stringify(store.getState())}`);
+
+  const upcomingState = [action].reduce(counterReducer, store.getState());
+  console.log(JSON.stringify(upcomingState));
+
+  //pass action
+  return next(action);
+};
 
 const store = createStore(counterReducer, applyMiddleware(myLogger));
 
