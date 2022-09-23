f7d618014d3db9d019dc6f632ccb35da7a289de8
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-23 23:23:28 +0600

    Node added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { createStore } from "redux";
 import counterReducer from "./counter/counterReducer";
 
+//create our first middleware
+
 const store = createStore(counterReducer);
 
 export default store;
