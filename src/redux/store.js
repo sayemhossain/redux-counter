41cf66b14898623889313e89498bbb882e2310b5
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-25 22:24:06 +0600

    new cdoea dded

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,11 @@ import { applyMiddleware, createStore } from "redux";
 import counterReducer from "./counter/counterReducer";
 import myLogger from "./middlewares/myLogger";
 import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(counterReducer, applyMiddleware(logger, myLogger));
+const store = createStore(
+  counterReducer,
+  composeWithDevTools(applyMiddleware(logger, myLogger))
+);
 
 export default store;
